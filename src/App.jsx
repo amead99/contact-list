47f884da942ae0f45abe3cb0172c5c90bc207332
Allison Meadors <allisonmeadors@Allisons-MacBook-Pro.local>
2024-01-04 15:02:19 -0600

    Completed semantic table structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import ContactList from './components/ContactList'
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <ContactList />
     </>
   )
 }
-
-export default App
